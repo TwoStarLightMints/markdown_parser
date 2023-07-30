2e83b66ec0ae4778fd7c24db1b4d558e705aac55
known <mondo.beyondo3@gmail.com>
2023-07-29 21:20:10 -0400

    Implement initial parsing behavior for headers

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    print("Hello, World!")
+SPECIAL_CHARACTERS = [
+    "#"
+]
+
+with open("markdown_doc.md", "r") as m:
+    content = m.readlines()
+
+parsed = list()
+
+for line in content:
+    if line.startswith("#"):
+        final = line.rfind("#")        
+
+        parsed.append(f"<h{final+1}>{line[final+2:]}</h{final+1}>")
+
+print(parsed)
