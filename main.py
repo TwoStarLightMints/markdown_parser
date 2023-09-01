bdeb0e94811cbc10e4291da81ed8c1f2a03f8401
known <mondo.beyondo3@gmail.com>
2023-09-01 17:20:28 -0400

    Change approach to tree type, starting with node class

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-SPECIAL_CHARACTERS = [
-    "#"
-]
-
-with open("markdown_doc.md", "r") as m:
-    content = m.readlines()
-
-parsed = list()
-
-for line in content:
-    if line.startswith("#"):
-        final = line.rfind("#")        
-
-        parsed.append(f"<h{final+1}>{line[final+2:]}</h{final+1}>")
-
-    elif line is "---" or line is "***" or line is "___":
-        parsed.append("<hr>")
-
-    else:
-        parsed.append(f"<p>{line}</p>")
-
-print(parsed)
+class Node:
+    def __init__(self):
+        self.children = list()
